6d13b6040416dcef1c79feddf086621dbad59892
Jada Douthit <jdouthit2022@gmail.com>
2025-06-05 14:21:05 -0400

    jk

--- Jada.R
+++ Jada.R
@@ -1,3 +1,9 @@
 library(tidyverse)
 nfl_passes <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/nfl_passes.csv")
 colnames(nfl_passes)
+table(nfl_passes$passer_player_name)
+names(nfl_passes)
+nfl_passes |>
+  arrange(play_id)
+
+count(nfl_passes)
