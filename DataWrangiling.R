afc1ddf63fbce94e764051d9012516a28bb70a5a
Sheryl Solorzano <ssolorzano27@amherst.edu>
2025-06-06 14:40:07 -0400

    first steps

--- DataWrangiling.R
+++ DataWrangiling.R
@@ -1,4 +1,8 @@
 library(tidyverse)
+library(kableExtra)
+
+
+
 nfl_passes <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/nfl_passes.csv")
 colnames(nfl_passes)
 table(nfl_passes$passer_player_name)
@@ -8,3 +12,57 @@ nfl_passes |>
 
 count(nfl_passes)
 view(nfl_passes)
+
+
+names(nfl_passes)
+summary(nfl_passes)
+
+
+nfl2 <- nfl_passes |>
+  select(passer_player_name, complete_pass) |>
+  group_by(passer_player_name) |>
+  summarize(total_pass = sum(complete_pass))
+
+passes <- nfl_passes |>
+  count(passer_player_name)
+
+names(passes)[names(passes) == "n"] <- "total_passes"
+passes$completed_passes <- nfl2$total_pass
+
+
+nfl1 <- nfl_passes |>
+  mutate(attempted_pass = sum(completed_passes)/ sum(total_passes))
+
+
+#completion percentage per season for all teams.
+
+ggplot(data = nfl_passes) +
+  geom_bar(aes(x = interception)) +
+  labs(title = "Whether passes were intercepted or not")
+
+
+ggplot(data = nfl_passes) +
+  geom_bar(aes(y = offense_formation)) +
+  labs(title = "Offense Formation",
+       y = "Offense Formation Type") 
+
+
+passes |>
+  ggplot(aes(x = total_passes)) +
+  geom_histogram()
+
+summary(passes$total_passes)
+
+# Top 10 players with the highest completion rate in terms of passes
+
+passes <- passes |>
+  filter(total_passes >= 100) |>
+  mutate(completion_percentage = completed_passes/ total_passes) |>
+  arrange(desc(completion_percentage))
+
+
+head(passes$passer_player_name, 10)
+
+
+
+
